4de455710ee1c563e5afa8c3877e1742761297a4
Jon Aksel <jonakselm@gmail.com>
2020-03-30 09:10:17 +0200

    removed warnings from Clock.hpp

--- include/Wrapper/System/Clock.hpp
+++ include/Wrapper/System/Clock.hpp
@@ -2,6 +2,9 @@
 #include <Wrapper/Export/Export.hpp>
 #include <chrono>
 
+template class GDIWRAPPER_API std::chrono::duration<__int64, std::nano>;
+template class GDIWRAPPER_API std::chrono::time_point<std::chrono::steady_clock, std::chrono::nanoseconds>;
+
 class GDIWRAPPER_API Clock
 {
 public:
